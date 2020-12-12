78adc95b002a58ce8b8e91cace291d45236af61f
ky28059 <60120929+ky28059@users.noreply.github.com>
2020-12-11 18:04:54 -0800

    Day 11

---
+++ src/AdventOfCode/Day 11 Part 1.kt
@@ -0,0 +1,49 @@
+package AdventOfCode
+
+
+fun main() {
+    println(recurseThroughCGOL(day11.toMutableList()))
+}
+
+tailrec fun recurseThroughCGOL(currentSeats: MutableList<String>): Int {
+    val temp = currentSeats.toMutableList()
+
+    for (rowNum in currentSeats.indices) {
+        val row = currentSeats[rowNum]
+
+        for (column in row.indices) {
+            when (row[column]) {
+                '.' -> {} // Do nothing if floor seat
+
+                // Probably better way of replacing chars in a string than replaceRange
+                // But I cannot reassign them directly using string[n] = x so whatever
+                'L' -> if (checkOccupiedNeighbors(rowNum, column, currentSeats) == 0) temp[rowNum] = temp[rowNum].replaceRange(column, column + 1, "#")
+                '#' -> if (checkOccupiedNeighbors(rowNum, column, currentSeats) >= 4) temp[rowNum] = temp[rowNum].replaceRange(column, column + 1, "L")
+            }
+        }
+    }
+    // If the next iteration is the same as the previous, print occupied seats
+    if (temp == currentSeats) return currentSeats.joinToString().filter { it == '#' }.length
+
+    return recurseThroughCGOL(temp)
+}
+
+fun checkOccupiedNeighbors(row: Int, col: Int, source: List<String>): Int {
+    var neighbors = 0
+    var rowMin = -1; var rowMax = 1
+    var colMin = -1; var colMax = 1
+
+    // Check ranges to catch outOfBounds exceptions
+    // Inelegant
+    if (row == 0) rowMin = 0
+    if (col == 0) colMin = 0
+    if (row == source.size - 1) rowMax = 0
+    if (col == source[row].length - 1) colMax = 0
+
+    for (i in rowMin..rowMax) {
+        for (k in colMin..colMax) {
+            if (source[row + i][col + k] == '#' && (i != 0 || k != 0)) neighbors++
+        }
+    }
+    return neighbors
+}
---
+++ src/AdventOfCode/Day 11 Part 2.kt
@@ -0,0 +1,54 @@
+package AdventOfCode
+
+
+fun main() {
+    println(recurseThroughCGOL2(day11.toMutableList()))
+}
+
+tailrec fun recurseThroughCGOL2(currentSeats: MutableList<String>): Int {
+    val temp = currentSeats.toMutableList()
+
+    for (rowNum in currentSeats.indices) {
+        val row = currentSeats[rowNum]
+
+        for (column in row.indices) {
+            when (row[column]) {
+                '.' -> {} // Do nothing if floor seat
+
+                // Probably better way of replacing chars in a string than replaceRange
+                // But I cannot reassign them directly using string[n] = x so whatever
+                'L' -> if (checkOccupiedNeighbors2(rowNum, column, currentSeats) == 0) temp[rowNum] = temp[rowNum].replaceRange(column, column + 1, "#")
+                '#' -> if (checkOccupiedNeighbors2(rowNum, column, currentSeats) >= 5) temp[rowNum] = temp[rowNum].replaceRange(column, column + 1, "L")
+            }
+        }
+    }
+    // If the next iteration is the same as the previous, print occupied seats
+    if (temp == currentSeats) return currentSeats.joinToString().filter { it == '#' }.length
+
+    return recurseThroughCGOL2(temp)
+}
+
+fun checkOccupiedNeighbors2(row: Int, col: Int, source: List<String>): Int {
+    var neighbors = 0
+
+    for (rise in -1..1) {
+        for (run in -1..1) { // run
+            if (!(rise == 0 && run == 0)) neighbors += rayCast(row, col, rise, run, source)
+        }
+    }
+    return neighbors
+}
+
+tailrec fun rayCast(y: Int, x: Int, rise: Int, run: Int, source: List<String>): Int {
+    try {
+        // If the chair exists
+        when (source[y + rise][x + run]) {
+            '#' -> return 1
+            'L' -> return 0
+        }
+    } catch (e: Exception) {
+        // If it throws out of bounds exception, you know there were no chairs in the line
+        return 0
+    }
+    return rayCast(y + rise, x + run, rise, run, source)
+}
