9677473d7de20ccf7c114c32d753c96340244c3f
Kevin Yu <kevinyu567@gmail.com>
2023-01-05 18:29:20 -0800

    Abstract day 6 more

--- src/AdventOfCode2022/Day 6 Part 1.kt
+++ src/AdventOfCode2022/Day 6 Part 1.kt
@@ -2,12 +2,15 @@ package AdventOfCode2022
 
 
 fun main() {
-    for (i in 3 until day6.length) {
-        if (checkForMarker(i, 4)) {
-            println(i + 1)
-            break
-        }
+    println(findMarker(4))
+}
+
+// Returns the last index of a marker of the given size in the signal.
+fun findMarker(size: Int): Int {
+    for (i in size - 1 until day6.length) {
+        if (checkForMarker(i, size)) return i + 1
     }
+    return -1
 }
 
 // Checks the signal for a marker of a given size at a given index.
--- src/AdventOfCode2022/Day 6 Part 2.kt
+++ src/AdventOfCode2022/Day 6 Part 2.kt
@@ -2,10 +2,5 @@ package AdventOfCode2022
 
 
 fun main() {
-    for (i in 13 until day6.length) {
-        if (checkForMarker(i, 14)) {
-            println(i + 1)
-            break
-        }
-    }
+    println(findMarker(14))
 }
