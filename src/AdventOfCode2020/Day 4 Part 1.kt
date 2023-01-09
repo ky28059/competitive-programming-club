9294e90db96487fd32f1dbe9d85212ef6479450b
Kevin Yu <kevinyu567@gmail.com>
2023-01-08 23:42:23 -0800

    Rewrite 2020 day 4

--- src/AdventOfCode2020/Day 4 Part 1.kt
+++ src/AdventOfCode2020/Day 4 Part 1.kt
@@ -2,22 +2,6 @@ package AdventOfCode2020
 
 
 fun main() {
-    var valid = 0
-
-    for (passport in day4) {
-        val fields = passport.split(" ").map { it.split(":") }
-
-        if (
-            // Check to see if all fields are present
-            fields.any { it[0] == "ecl" }
-            && fields.any { it[0] == "pid" }
-            && fields.any { it[0] == "eyr" }
-            && fields.any { it[0] == "hcl" }
-            && fields.any { it[0] == "byr" }
-            && fields.any { it[0] == "iyr" }
-            && fields.any { it[0] == "hgt" }
-        ) valid++
-    }
-
-    println(valid)
+    val requiredFields = setOf("ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt")
+    println(day4.count { it.split(" ").map { f -> f.split(":")[0] }.containsAll(requiredFields) })
 }
--- src/AdventOfCode2020/Day 4 Part 2.kt
+++ src/AdventOfCode2020/Day 4 Part 2.kt
@@ -2,56 +2,44 @@ package AdventOfCode2020
 
 
 fun main() {
-    var valid = 0
+    println(day4.count { checkPassport(it) })
+}
 
-    for (passport in day4) {
-        val fields = passport.split(" ").map { it.split(":") }
-        var currValid = true
+// Checks whether a passport is legitimate (i.e. all fields are present and valid).
+// Better way to abstract checking for existence of fields?
+fun checkPassport(passport: String): Boolean {
+    val fields = passport.split(" ").map { it.split(":") }
 
-        for ((field, value) in fields) {
-            when (field) {
-                // Only reassign validity if it is true
-                // This way a later passed check will not override an already false passport
-                "byr" -> if (currValid) currValid = value.toIntOrNull() in 1920..2002 // Must be within range
-                "iyr" -> if (currValid) currValid = value.toIntOrNull() in 2010..2020 // Must be within range
-                "eyr" -> if (currValid) currValid = value.toIntOrNull() in 2020..2030 // Must be within range
-                "hgt" ->
-                    if (currValid) {
-                        val unit = value.substring(value.length - 2, value.length) // Unit = last 2 chars of string
-                        val (min, max) = when (unit) {
-                            "cm" -> listOf(150, 193)
-                            else -> listOf(59, 76) // "in"
-                        }
-                        currValid = value.substring(0, value.length - 2).toIntOrNull() in min..max // Int given must be within range specified by unit
-                    }
-                "hcl" ->
-                    if (currValid) currValid =
-                        value[0] == '#' // First letter must be a #
-                        && value.substring(1, value.length).all { it in 'a'..'f' || it.digitToIntOrNull() in 0..9 } // All characters after the # must either be a-f or 0-9
-                        && value.length == 7 // Must be exactly 7 characters
-                "ecl" -> if (currValid) currValid = listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(value) // Must be one of the specified strings
-                "pid" -> if (currValid) currValid = value.toIntOrNull() != null && value.length == 9 // Must be a 9 digit valid integer
-            }
-        }
+    // Birth year: between 1920-2002
+    val (_, byr) = fields.find { it[0] == "byr" } ?: return false
+    if (byr.toIntOrNull() !in 1920..2002) return false
 
-        // Might be nicer to do search in the checks when statement but oh well, I already wrote this code
-        fun isInField(query: String): Boolean {
-            val search = fields.map { it[0] == query }
-            return search.reduce { s1, s2 -> s1 || s2 }
-        }
+    // Issue year: between 2010-2020
+    val (_, iyr) = fields.find { it[0] == "iyr" } ?: return false
+    if (iyr.toIntOrNull() !in 2010..2020) return false
 
-        if (
-            // Check to see if all fields are present
-            isInField("ecl")
-            && isInField("pid")
-            && isInField("eyr")
-            && isInField("hcl")
-            && isInField("byr")
-            && isInField("iyr")
-            && isInField("hgt")
-            && currValid // Lazily check to see if it passed all the other checks
-        ) valid++
+    // Expiration year: between 2020-2030
+    val (_, eyr) = fields.find { it[0] == "eyr" } ?: return false
+    if (eyr.toIntOrNull() !in 2020..2030) return false
+
+    // Height: must be within unit-dependant bounds
+    val (_, hgt) = fields.find { it[0] == "hgt" } ?: return false
+    val unit = hgt.takeLast(2)
+    val (min, max) = when (unit) {
+        "cm" -> listOf(150, 193)
+        else -> listOf(59, 76) // "in"
     }
+    if (hgt.dropLast(2).toIntOrNull() !in min..max) return false
 
-    println(valid)
+    // Hair color: must be valid 6-digit hex
+    val (_, hcl) = fields.find { it[0] == "hcl" } ?: return false
+    if (!hcl.matches(Regex("#[a-f0-9]{6}"))) return false
+
+    // Eye color: must be valid option
+    val (_, ecl) = fields.find { it[0] == "ecl" } ?: return false
+    if (ecl !in setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")) return false
+
+    // Passport ID: must be 9-digit number
+    val (_, pid) = fields.find { it[0] == "pid" } ?: return false
+    return pid.length == 9 && pid.toIntOrNull() != null
 }
