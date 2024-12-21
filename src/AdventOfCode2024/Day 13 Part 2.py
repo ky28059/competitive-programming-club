9fc3f6d12f72a66bea660b9995bf02b4783b7950
Kevin Yu <kevinyu567@gmail.com>
2024-12-20 17:24:40 -0800

    Day 13

---
+++ src/AdventOfCode2024/Day 13 Part 2.py
@@ -0,0 +1,27 @@
+import re
+
+import numpy as np
+
+
+with open('./day13.in') as f:
+    groups = [
+        re.match(r'Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)', l).groups()
+        for l in f.read().split('\n\n')
+    ]
+
+sum = 0
+
+for ax, ay, bx, by, tx, ty in groups:
+    (x,), (y,) = np.linalg.solve(
+        np.array([[int(ax), int(bx)], [int(ay), int(by)]]),
+        np.array([[int(tx) + 10000000000000], [int(ty) + 10000000000000]])
+    )
+
+    # Discard non-solved equations, accounting for floating point errors
+    if 0.9999 > x % 1 > 0.0001 or 0.9999 > y % 1 > 0.0001:
+        continue
+
+    a, b = round(x), round(y)
+    sum += a * 3 + b
+
+print(sum)
